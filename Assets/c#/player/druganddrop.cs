666815936895a67ffd143739e63c66d6ea7f168a
Ivankulckin <ivancoolstudio1@gmail.com>
2024-04-10 12:10:42 +0400

    я не помню что я добавил

--- Assets/c#/player/druganddrop.cs
+++ Assets/c#/player/druganddrop.cs
@@ -1,74 +1,33 @@
 using UnityEngine;
- 
-[RequireComponent(typeof(Rigidbody2D))]
-public class druganddrop : MonoBehaviour
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class druganddrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    private bool _isDraged = false;
+    private RectTransform rectTransform;
+    private Image image;
 
-    private Plane _planeXY;
- 
-    private Rigidbody2D _rb2D;
-    private HingeJoint2D _hindleJoint2D;
- 
-    void Start()
+    private void Awake()
     {
-        _rb2D = GetComponent<Rigidbody2D>();
-        _rb2D.isKinematic = true;
- 
-        _isDraged = false;
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
     }
- 
-    void Update()
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        // начинаем перетаскивание, если нажали на объект с тегом "seed" в сцене и нет другого перетаскиваемого объекта
-        if (Input.GetMouseButtonDown(0) && _isDraged == false)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit2D = Physics2D.GetRayIntersection(ray);
-
-            if (hit2D.collider != null && hit2D.collider.CompareTag("seed"))
-            {
-                _planeXY = new Plane(Vector3.forward, hit2D.transform.position);
-
-                _rb2D.position = hit2D.point;
-
-                _hindleJoint2D = hit2D.transform.gameObject.AddComponent<HingeJoint2D>();
-                _hindleJoint2D.connectedBody = this._rb2D;
- 
-                _isDraged = true;
-            }
-        }
- 
-        // прекращаем перетаскивание при отпускании мыши
-        if (Input.GetMouseButtonUp(0) && _isDraged)
-        {
-            Destroy(_hindleJoint2D);
-            _isDraged = false;
-        }
- 
-        // реализанция перетаскивания
-        if (_isDraged)
-        {
-            if (_hindleJoint2D == null || _hindleJoint2D.gameObject == null)
-            {
-                _hindleJoint2D = null;
-                _isDraged = false;
-                return;
-            }
- 
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (_planeXY.Raycast(ray, out float enterPoint))
-            {
-                _rb2D.position = ray.GetPoint(enterPoint);
-            }
-        }
+        image.color = new Color(0f, 255f, 200f, 0.7f);
+        image.raycastTarget = false;
     }
- 
-    private void OnDestroy()
+
+    public void OnDrag(PointerEventData eventData)
     {
-        if (_hindleJoint2D != null && _hindleJoint2D.gameObject != null)
-        {
-            Destroy(_hindleJoint2D);
-        }
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.color = new Color(255f, 255f, 255f, 1f);
+        image.raycastTarget = true;
     }
 }
